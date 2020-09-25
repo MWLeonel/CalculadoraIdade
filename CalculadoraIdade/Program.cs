bcbb1a007c605c1fc74c1c9cdafbf73daf3c2983
MWLeonel <wmleonel6@gmail.com>
2020-09-25 00:26:58 -0300

    Calcular a Idade e mostrar o Resultado em Dias

---
+++ CalculadoraIdade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalculadoraIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataNascimento;
+            DateTime dataAtual;
+            TimeSpan result;
+
+            Console.Write("Digite sua Data de Nascimento: ");
+            dataNascimento = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Data Atual: ");
+            dataAtual = DateTime.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            result = dataAtual - dataNascimento;
+            Console.WriteLine($"Você tem {result.Days} dias");
+
+            Console.ReadKey();
+        }
+    }
+}
